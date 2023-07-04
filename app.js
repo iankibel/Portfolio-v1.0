60627b5f994e8ca193dbcfc37ded259d0600bc07
Ian Kibel <ian.kibel@gmail.com>
2023-07-04 10:30:17 -0300

    Avance mediano

---
+++ app.js
@@ -0,0 +1,11 @@
+const openAside = document.querySelector('#open-aside')
+const closeAside = document.querySelector('#close-aside')
+const aside = document.querySelector('#aside')
+
+openAside.addEventListener("click", () => {
+    aside.classList.add("active")
+})
+  
+closeAside.addEventListener("click", () => {
+    aside.classList.remove("active")
+})

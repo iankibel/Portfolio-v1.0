69cadede109571de0223f8ee31631556d4c2c71f
Ian Kibel <ian.kibel@gmail.com>
2023-07-07 12:45:30 -0300

    Final commit v2 (finish + themes + lang)

--- JS/app.js
+++ JS/app.js
@@ -11,6 +11,34 @@ closeAside.addEventListener("click", () => {
   aside.classList.remove("aside-opened")
 })
 
+// 
+const asideGoTo0 = document.querySelector("#aside-goto0")
+const asideGoTo1 = document.querySelector("#aside-goto1")
+const asideGoTo2 = document.querySelector("#aside-goto2")
+const asideGoTo3 = document.querySelector("#aside-goto3")
+const asideGoTo4 = document.querySelector("#aside-goto4")
+
+asideGoTo0.addEventListener("click", () => {
+    aside.classList.remove("aside-opened")
+})
+
+asideGoTo1.addEventListener("click", () => {
+  aside.classList.remove("aside-opened")
+})
+
+asideGoTo2.addEventListener("click", () => {
+  aside.classList.remove("aside-opened")
+})
+
+asideGoTo3.addEventListener("click", () => {
+  aside.classList.remove("aside-opened")
+})
+
+asideGoTo4.addEventListener("click", () => {
+  aside.classList.remove("aside-opened")
+})
+
+
 // Change header on scroll
 window.onscroll = function() {scrollFunction()};
 
@@ -21,3 +49,20 @@ function scrollFunction() {
     document.getElementById("header").classList.remove("header-scroll");
   }
 }
+
+// 
+
+const colorSchemePrefered = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const changeTheme = document.getElementById('btn-change-theme')
+
+const setTheme = (theme) => {
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme)
+}
+
+changeTheme.addEventListener("click", () => {
+  let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+  setTheme(switchToTheme);
+})
+
+setTheme(localStorage.getItem('theme') || colorSchemePrefered);
